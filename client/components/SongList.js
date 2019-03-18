9b43418e8901ae114b21fa366e8c557df71703b5
Vlad <vovaturcan4@gmail.com>
2019-03-19 00:04:34 +0200

    solved a bug with mongoose andusing now graphql through apollo on client

---
+++ client/components/SongList.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => {
+      return (
+        <li className="collection-item" key={song.id}>
+          {song.title}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    console.log(this.props);
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return <ul className="collection">{this.renderSongs()}</ul>;
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      title
+      id
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
